42101ae1cc0249775a18658d00deef04ed0cc522
Jessica Yung <jy357@cam.ac.uk>
2016-10-15 10:54:54 +0100

    test: add sample unit test and function, write test (buggy) for p2.
    - chore: change file names to match Python style guide (change dashes to underscores)
    
    ISSUE: p2 tests buggy. I deliberately included assertEquals(0, 1) so it should throw an assertion error but it's not running any of the tests.

---
+++ mathematics/project-euler/example_code_with_unittest.py
@@ -0,0 +1,23 @@
+"""
+A simple function and a unit test to go with it.
+Have tested that this returns no errors with the uncommented tests and that
+it returns an AssertionError when the commented (deliberately false) test is included.
+
+"""
+
+import unittest
+
+def square_x(x):
+    """Square X."""
+    return x**2
+
+class Tests(unittest.TestCase):
+    """Unit test for `square_x` function."""
+
+    def test_square_x(self):
+        self.assertEqual(square_x(4), 16)
+        self.assertEqual(square_x(10), 100)
+#        self.assertEqual(square_x(1), -1)
+
+if __name__ == '__main__':
+    unittest.main()
