5eb45d4df0a5ee7145a9e3430dbee2ec40fb689a
Jessica Yung <jy357@cam.ac.uk>
2016-11-22 08:29:37 +0000

    feat(icake): add p37 rand5() from rand7()

---
+++ interview-cake/p37.py
@@ -0,0 +1,28 @@
+"""
+You have a function rand7() that generates a random integer from 1 to 7. Use it to write a function rand5() that
+generates a random integer from 1 to 5.
+"""
+import numpy as np
+def rand7():
+    return int(np.random.random()*7+1)
+
+def rand5():
+    result = 7
+    while result > 5:
+        result = rand7()
+    return result
+
+def rand5_v1():
+    stop = False
+
+    while stop == False:
+        num = rand7()
+        if num <= 5:
+            stop = True
+            return num
+
+# Test rand7():
+"""
+for i in range(10):
+    print(rand7())
+"""
