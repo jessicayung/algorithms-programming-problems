d6fd3067cdf6a8d20ff7674f0b118c044418fb54
Jessica Yung <jessicayungyc@gmail.com>
2018-01-11 16:28:37 +0000

    feat(algs): add add_edges, fix remove_vertex in Graph class - add extra test case

--- algorithms/graph.py
+++ algorithms/graph.py
@@ -11,12 +11,13 @@ class Graph:
         self.adj_mat = adjacency_matrix
         self.v = num_vertices
 
-    def is_adjacent(v1, v2):
+    def is_adjacent(self, v1, v2):
         return self.adj_mat[v1, v2]
 
-    def remove_vertex(v):
-        self.adj_mat[v,:] = 0
-        self.adj_mat[:,v] = 0
+    def remove_vertex(self, v):
+        self.adj_mat = np.delete(self.adj_mat, v, 0)
+        self.adj_mat = np.delete(self.adj_mat, v, 1)
+        self.v -= 1
 
     def find_num_islands(self):
         num_islands = 0
@@ -34,7 +35,20 @@ class Graph:
         self.num_islands = num_islands
         return num_islands
 
+    def add_edges(self, edges):
+        """Edges = an array of length-two arrays, each length-two array representing an edge."""
+        for edge in edges:
+            i = edge[0]
+            j = edge[1]
+            self.adj_mat[i][j] = self.adj_mat[j][i] = 1
+
+
+
 # Test Graph class
 # t1 = Graph([[1,1,1,0],[1,1,0,0],[1,0,1,0],[0,0,0,1]], 4)
 # print(t1.find_num_islands())
 
+# t2 = Graph(np.zeros((6,6)),6)
+# edges = [[0,5],[2,4],[2,3],[3,4]]
+# t2.add_edges(edges)
+# print(t2.find_num_islands())
